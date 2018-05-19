781c462b4944b5305852dccf6bc3942675ffe658
Maksim Kunaev <maksimtruesim@gmail.com>
2018-05-20 02:14:33 +0700

    progress

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { bind } from 'decko';
 import Table from '../Table/Table';
 import Card from '../Card';
 import './App.css';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const SHORT_DATA_URL = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
 const LARGE_DATA_URL = 'http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
@@ -27,12 +28,13 @@ class App extends Component {
             sortDirection: 'desc',
             currentPage: 1,
             selectedUser: null,
-            pages: 1
+            pages: 1,
+            loaded: null
         };
     }
 
     componentDidMount() {
-        this.loadShortData();
+        this.loadLargeData();
         document.addEventListener('keydown', this.onKeyDown);
     }
 
@@ -47,7 +49,8 @@ class App extends Component {
 
         this.setState({
             fullUsers: data,
-            pages
+            pages,
+            loaded: true
         }, this.sort)
     }
 
@@ -180,17 +183,18 @@ class App extends Component {
         const {
             sortBy,
             sortDirection,
-            selectedUser } = this.state;
+            selectedUser,
+            loaded } = this.state;
         const btns = this.renderPagination();
         const tableData = this.getPageData();
+        let content;
 
-        return (
-            <div className='App'>
-                <form className='App__filter' onSubmit={this.onFilter}>
-                    <input ref={el => this.filterControl = el}/>
-                    <button>Найти</button>
-                </form>
-                <h1>USERS</h1>
+        if (!loaded) {
+            content = <div className='App__progress'>
+                <CircularProgress size={70} />
+            </div>
+        } else {
+            content = <div>
                 <Table
                     fields={FIELDS}
                     data={tableData}
@@ -200,6 +204,17 @@ class App extends Component {
                     onSelect={this.onSelect}/>
                 <Card data={selectedUser}/>
                 { btns.map(btn => btn)}
+            </div>
+        }
+
+        return (
+            <div className='App'>
+                <form className='App__filter' onSubmit={this.onFilter}>
+                    <input ref={el => this.filterControl = el}/>
+                    <button>Найти</button>
+                </form>
+                <h1>USERS</h1>
+                { content }
             </div>
         )
     }
